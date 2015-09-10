e8908a3e1bdee2dbaaf97a12c5ec2a6836b6f0e9
TeoChoi <709078822@qq.com>
2015-09-11 00:32:34 +0800

    初步完成版本

--- lib/HashIds.php
+++ lib/HashIds.php
@@ -5,8 +5,34 @@ use yii\base\Object;
 
 class Hashids extends Object
 {
+    /**
+     * @var string
+     */
+    public $hashClass = '\Hashids\Hashids';
+
+    /**
+     * @var array
+     */
+    public $hashProperty = [];
+
+    private $_instance = null;
+
     public function init()
     {
+        $this->_instance = (new \ReflectionClass($this->hashClass))->newInstanceArgs($this->hashProperty);
 
+        return parent::init();
+    }
+
+    /**
+     * @param string $method
+     * @param array $params
+     * @return mixed
+     */
+    public function __call($method, $params) {
+        if (method_exists($this->_instance, $method)) {
+            call_user_func_array([$this->_instance, $method], $params);
+        }
+        return parent::__call($method, $params);
     }
 }
