27400c53a294725a8a448bc7af2dcfc57d1e0c13
TeoChoi <709078822@qq.com>
2015-09-11 10:08:59 +0800

    修改整体逻辑

--- lib/HashIds.php
+++ lib/HashIds.php
@@ -1,38 +1,12 @@
 <?php
-namespace yii2\hashids;
+namespace yii2\object;
 
-use yii\base\Object;
 
-class Hashids extends Object
+class Hashids extends YiiObject
 {
     /**
      * @var string
      */
-    public $hashClass = '\Hashids\Hashids';
+    public $objectClass = '\Hashids\Hashids';
 
-    /**
-     * @var array
-     */
-    public $hashProperty = [];
-
-    private $_instance = null;
-
-    public function init()
-    {
-        $this->_instance = (new \ReflectionClass($this->hashClass))->newInstanceArgs($this->hashProperty);
-
-        return parent::init();
-    }
-
-    /**
-     * @param string $method
-     * @param array $params
-     * @return mixed
-     */
-    public function __call($method, $params) {
-        if (method_exists($this->_instance, $method)) {
-            call_user_func_array([$this->_instance, $method], $params);
-        }
-        return parent::__call($method, $params);
-    }
 }
